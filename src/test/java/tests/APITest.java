3aec041bbb1b93c024487152ecf56fec490cf838
James Snee <james.snee@accesshq.com>
2024-08-20 10:49:51 +1000

    Added assert to API test

--- src/test/java/tests/APITest.java
+++ src/test/java/tests/APITest.java
@@ -3,6 +3,7 @@ package tests;
 import api.CatResponse;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class APITest extends BaseTest {
@@ -10,7 +11,7 @@ public class APITest extends BaseTest {
     @Test
     public void exampleAPITest() {
         Response response = RestAssured.given().when().get(baseAPIUrl);
-        response.then().statusCode(200);
+        Assert.assertEquals(response.getStatusCode(), 200, "Error: Expected status code to be 200!");
         System.out.println("Raw response: " + response.body().asString());
         CatResponse catResponse = response.as(CatResponse.class);
         System.out.println("Cat Fact: " + catResponse.getFact());
